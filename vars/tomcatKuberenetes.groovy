e3231bb473eab49406af725720d111c69d705d80
keerthana2022 <101049858+keerthana2022@users.noreply.github.com>
2022-04-17 21:22:08 +0530

    Create tomcatKuberenetes.groovy

---
+++ vars/tomcatKuberenetes.groovy
@@ -0,0 +1,27 @@
+def test( String dregistryin = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a',) {
+
+pipeline {
+environment { 
+		registry = "$dregistryin" 	
+		dockerTag = "${docTag}$BUILD_NUMBER"
+		gitRepo = "${grepo}"
+		gitBranch = "${gbranch}"
+	}
+	
+	agent none 
+	 stages {
+		stage("Checkout"){
+      agent{label 'docker1'}
+			steps {
+			
+			    checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/keerthana2022/newtestrepo.git']]])
+				 //checkout([$class: 'GitSCM', branches: [[name: "$gitBranh"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])
+			}
+		}
+		stage("Build"){
+      agent{label 'docker1'}
+			steps {
+			
+			    sh 'docker build -t "$registry:$dockerTag" .'
+			}
+		}
